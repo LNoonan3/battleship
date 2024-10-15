c651bf5fea0d50832e6f4d7c662b39b82becd6e1
Lucy Noonan <lucynoonan3@gmail.com>
2024-10-15 14:30:49 +0000

    implement a class to represent a players grid in the game

--- run.py
+++ run.py
@@ -13,5 +13,17 @@ class Ship:
         self.hits = 0
 
     def is_sunk(self):
-        """Check if the ship is sunk."""
+        """
+        Check if the ship is sunk.
+        """
         return self.hits == self.size
+
+class Grid:
+    """
+    A class representing a player's grid in the game of Battleship.
+    """
+
+    def __init__(self, size):
+        self.size = size
+        self.grid = [['~' for _ in range(size)] for _ in range(size)]
+        self.ships = []

56795455b05527412d8b1d02be92e703aee48ca9
Lucy Noonan <lucynoonan3@gmail.com>
2024-10-16 14:24:32 +0000

    implement function to randomly place ships and the '~' on the grid

--- run.py
+++ run.py
@@ -7,6 +7,9 @@ class Ship:
     """
 
     def __init__(self, name, size):
+        """
+        Initializes the ships object with a name, size, and empty coordinates list.
+        """
         self.name = name
         self.size = size
         self.coordinates = []
@@ -14,7 +17,7 @@ class Ship:
 
     def is_sunk(self):
         """
-        Check if the ship is sunk.
+        Check if the ship is sunk (i.e., if the number of hits equals the size of the ship.)
         """
         return self.hits == self.size
 
@@ -24,6 +27,9 @@ class Grid:
     """
 
     def __init__(self, size):
+        """
+        Initializes the Grid onject with a given size and an empty grid of water (~).
+        """
         self.size = size
         self.grid = [['~' for _ in range(size)] for _ in range(size)]
         self.ships = []
@@ -45,3 +51,28 @@ class Grid:
             
             #Adjust row numbers to aligin with single/double digits
             print(f"{i+1<2} " + " ".join(row_display))
+
+    def place_ship(self, ship):
+        """
+        Places a ship randomly on the grid.
+        """
+        valid_placement = False
+        while not valid_placement:
+            orientation = random.choice(['H', 'V'])
+            if orientation == 'H':
+                row = random.randint(0, self.size - 1)
+                col = random.randint(0, self.size - ship.size)
+                if all(self.grid[row][col + i] == '~' for i in range(ship.size)):
+                    for i in range(ship.size):
+                        self.grid[row][col + i] = 'S'
+                        ship.coordinates.append((row, col + i))
+                    valid_placement = True
+            else:
+                row = random.randint(0, self.size - ship.size)
+                col = random.randint(0, self.size - 1)
+                if all(self.grid[row + i][col] == '~' for i in range(ship.size)):
+                    for i in range(ship.size):
+                        self.grid[row + i][col] == 'S'
+                        ship.coordinates.append((row + i, col))
+                    valid_placement = True
+        self.ships.append(ship)
